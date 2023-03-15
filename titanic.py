b2905d58384bf64ecfc44a793df8d61f121604ea
Vasallo94 <65018705+Vasallo94@users.noreply.github.com>
2023-03-15 20:59:37 +0100

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -1,6 +1,6 @@
 # --------------------LIBRERÍAS----------------------------#
 import streamlit as st
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
